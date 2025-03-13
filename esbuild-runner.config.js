5cca5e4c2651473a3ab789fad8a2b7b7bad26bc3
Luke Harold Miles <10591373+qpwo@users.noreply.github.com>
2025-03-12 19:39:22 -0500

    fix typo

--- esbuild-runner.config.js
+++ esbuild-runner.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   target: "node16",
   format: "cjs",
-  externalModules: ["uWebSockets.js", "ws"],
+  external: ["uWebSockets.js", "ws"],
   define: {
     "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development")
   }
