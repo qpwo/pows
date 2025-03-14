846f280f0437f1182a92a121c0bbc72c74edf91a
Luke Harold Miles <10591373+qpwo@users.noreply.github.com>
2025-03-14 14:50:54 -0500

    sleep in client

--- example-big-client.ts
+++ example-big-client.ts
@@ -1,23 +1,28 @@
 
 // example-big-client.ts
-import { connectTo } from './tsws-node-client';
-import type { Routes } from './example-big-server';
+import { connectTo } from './tsws-node-client'
+import type { Routes } from './example-big-server'
 
 const api = connectTo<Routes, {}>({
   approve: async (question) => {
-    return confirm(question);
+    return confirm(question)
   },
 }, {
   url: 'ws://localhost:8080',
-});
+})
 
 async function main() {
-  console.log('Square(5):', await api.server.procs.square(5));
-  console.log('Who am I?:', await api.server.procs.whoami());
+  await sleep()
+  console.log('Square(5):', await api.server.procs.square(5))
+  console.log('Who am I?:', await api.server.procs.whoami())
 
   for await (const update of api.server.streamers.doBigJob()) {
-    console.log('Job status:', update);
+    console.log('Job status:', update)
   }
+}
+
+async function sleep({ ms = 1000 } = {}) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 main()
